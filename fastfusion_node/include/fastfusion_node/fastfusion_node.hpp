f425c99f02f6e3fe27a29304f2d2ec5ad1739ed8
karrerm <karrerm@student.ethz.ch>
2016-03-17 12:45:23 +0100

    Commenting and delete unnecessary members...

--- fastfusion_node/include/fastfusion_node/fastfusion_node.hpp
+++ fastfusion_node/include/fastfusion_node/fastfusion_node.hpp
@@ -2,7 +2,7 @@
  * fastfusion_node.hpp
  *
  *  Created on: Sep 27, 2015
- *      Author: karrer
+ *      Author: karrerm
  */
 
 #ifndef INCLUDE_FASTFUSION_NODE_HPP_
@@ -31,6 +31,7 @@
 
 #include <pcl_ros/point_cloud.h>
 #include <pcl_conversions/pcl_conversions.h>
+
 class FastFusionWrapper {
 public:
 	FastFusionWrapper(void);
@@ -38,23 +39,23 @@ public:
 	//-- Main Function
 	void run(void);
 protected:
-	//-- Image Message Callback
+	//-- Image Message Callback (not used)
 	void imageCallback(const sensor_msgs::ImageConstPtr& msg_cam0, const sensor_msgs::ImageConstPtr& msg_cam1);
-	void imageCallbackPicoSLAM(const sensor_msgs::ImageConstPtr& msgDepth, const sensor_msgs::ImageConstPtr& msgConf,
-			const sensor_msgs::ImageConstPtr& msgNoise, const nav_msgs::Odometry::ConstPtr& msgOdom);
+	//-- ToF Callback with noise data
 	void imageCallbackPico(const sensor_msgs::ImageConstPtr& msgDepth, const sensor_msgs::ImageConstPtr& msgConf,
 				const sensor_msgs::ImageConstPtr& msgNoise);
-	void imageCallbackPicoSLAM(const sensor_msgs::ImageConstPtr& msgDepth, const sensor_msgs::ImageConstPtr& msgConf,
-			const nav_msgs::Odometry::ConstPtr& msgOdom);
+	//-- ToF Callback without noise data
 	void imageCallbackPico(const sensor_msgs::ImageConstPtr& msgDepth, const sensor_msgs::ImageConstPtr& msgConf);
-	void pclCallback(sensor_msgs::PointCloud2 pcl_msg);
+	//-- Registered Point cloud callback (colored pointcloud) for the use with the RealSense sensor
+	void registerPointCloudCallback(const sensor_msgs::PointCloud2::ConstPtr & pcl_msg);
+
+	//-- Extract OpenCV mat from ROS messages
 	void getRGBImageFromRosMsg(const sensor_msgs::ImageConstPtr& msgRGB, cv::Mat *rgbImg);
 	void getConfImageFromRosMsg(const sensor_msgs::ImageConstPtr& msgConf, cv::Mat *confImg);
 	void getDepthImageFromRosMsg(const sensor_msgs::ImageConstPtr& msgDepth, cv::Mat *dephtImg);
 	void getNoiseImageFromRosMsg(const sensor_msgs::ImageConstPtr& msgNoise, cv::Mat *noiseImg);
-	void registerPointCloudCallbackSLAM(const sensor_msgs::PointCloud2::ConstPtr & pcl_msg,
-			const nav_msgs::Odometry::ConstPtr& msgOdom);
-	void registerPointCloudCallback(const sensor_msgs::PointCloud2::ConstPtr & pcl_msg);
+
+	//-- Parametetric depth correction
 	void depthImageCorrection(cv::Mat & imgDepth, cv::Mat * imgDepthCorrected);
 
 	//-- Color encoding
@@ -63,6 +64,7 @@ protected:
 	//-- Pose Message to eigen (Rotation Matrix + Translation)
 	tf::TransformListener tfListener;
 	CameraInfo convertTFtoCameraInfo(const tf::Transform& transform);
+
 	//-- ROS node handle
 	ros::NodeHandle node_, nodeLocal_;
 	ros::Time previous_ts_;
@@ -71,47 +73,44 @@ protected:
 	cv::Mat depthCorrection_;
 	double imageScale_;
 
+	//-- Wrapper Member for the actual fusion
 	OnlineFusionROS onlinefusion_;
+
 	//-- Subscribers
 	message_filters::Subscriber<sensor_msgs::Image> *subscriberRGB_;
 	message_filters::Subscriber<sensor_msgs::Image> *subscriberDepth_;
 	message_filters::Subscriber<sensor_msgs::Image> *subscriberNoise_;
 	message_filters::Subscriber<sensor_msgs::Image> *subscriberConfidence_;
-	message_filters::Subscriber<nav_msgs::Odometry> *subscriberOdometry_;
 	message_filters::Subscriber<sensor_msgs::PointCloud2> *subscriberPointCloud_;
-	message_filters::Synchronizer<message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image,
-			sensor_msgs::Image, nav_msgs::Odometry> > *syncNoiseSLAM_;
+	//-- Synchronizer
 	message_filters::Synchronizer<message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image,
 				sensor_msgs::Image> > *syncNoise_;
-	message_filters::Synchronizer<message_filters::sync_policies::ApproximateTime<sensor_msgs::Image,sensor_msgs::Image, nav_msgs::Odometry> > *syncSLAM_;
 	message_filters::Synchronizer<message_filters::sync_policies::ApproximateTime<sensor_msgs::Image,sensor_msgs::Image> > *sync_;
-	message_filters::Synchronizer<message_filters::sync_policies::ApproximateTime<sensor_msgs::PointCloud2, nav_msgs::Odometry> > *syncPointCloudSLAM_;
+
+	//-- TF related Members
 	void broadcastTFchain(ros::Time timestamp);
 	tf::TransformBroadcaster tfBroadcaster_;
 	tf::Transform tf_cam0_imu;
 	tf::Transform tf_depth_cam0;				// When using ToF camera
-	tf::Transform tf_rgb_cam0;				// When using Realsense
-	tf::Transform tf_body_cam;				// Transfrom VICON-Body to imu
+	tf::Transform tf_rgb_cam0;					// When using Realsense
+	tf::Transform tf_body_cam;					// Transfrom VICON-Body to imu
 	Eigen::Matrix3d R_cam0_imu, R_depth_cam0, R_rgb_cam0, R_body_cam;
 	Eigen::Vector3d t_cam0_imu, t_depth_cam0, t_rgb_cam0, t_body_cam;
 	std::string world_id_;
 	std::string cam_id_;
 
+	//-- Should currently be mapped or not?
 	bool runMapping_;
+
+	//-- Parameters for configuration
 	int frameCounter_;
 	bool use_pmd_, depth_noise_;
 	bool testing_point_cloud_;
 	pcl::PointCloud<pcl::PointXYZRGB> pointCloudFrame_;
 	pcl::PointCloud<pcl::PointXYZRGB> pointCloudFrameTrans_;
 
-	//-- Running estimate for decay time
-	bool useSlam_;
-	double *latestDerivativeSingularVals_;
-	double lastSingularVal_;
-	int singValCounter_;
+	//-- Decay Time for the use with time-window based reconstruction
+	double decayTime_;
 };
-
-
-
 
 #endif /* INCLUDE_FASTFUSION_NODE_HPP_ */
